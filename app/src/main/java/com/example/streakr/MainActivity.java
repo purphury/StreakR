df14ecd46d7179a68b22f9097cfd385fc1be21e2
daniel-nix <dnix7774@gmail.com>
2019-04-13 18:50:04 -0500

    Kind of works. Actually opens Snapchat.

--- app/src/main/java/com/example/streakr/MainActivity.java
+++ app/src/main/java/com/example/streakr/MainActivity.java
@@ -5,23 +5,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.snapchat.kit.sdk.SnapCreative;
+import com.snapchat.kit.sdk.creative.api.SnapCreativeKitApi;
+import com.snapchat.kit.sdk.creative.exceptions.SnapMediaSizeException;
+import com.snapchat.kit.sdk.creative.media.SnapMediaFactory;
+import com.snapchat.kit.sdk.creative.media.SnapPhotoFile;
+import com.snapchat.kit.sdk.creative.models.SnapPhotoContent;
+
+import java.io.File;
+
 import pl.aprilapps.easyphotopicker.EasyImage;
 
 public class MainActivity extends AppCompatActivity {
-
+    SnapCreativeKitApi snapCreativeKitApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        EasyImage.configuration(this)
-//                .setAllowMultiplePickInGallery(false);
-//        Button sendButton = findViewById(R.id.sendPhotoButton);
-//        sendButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startPhotoSend();
-//            }
-//        });
+        snapCreativeKitApi = SnapCreative.getApi(this);
+        SnapMediaFactory snapMediaFactory = SnapCreative.getMediaFactory(this);
+        File lion = new File("lion.png");
+        try {
+            SnapPhotoFile photo = snapMediaFactory.getSnapPhotoFromFile(lion);
+            SnapPhotoContent snapPhotoContent = new SnapPhotoContent(photo);
+            snapCreativeKitApi.send(snapPhotoContent);
+        } catch (SnapMediaSizeException e) {
+            e.printStackTrace();
+        }
     }
 }
